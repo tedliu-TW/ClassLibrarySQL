0c87b03908ca5bf4a50543ffd1c3d3736259f1b2
劉敦桐 <zse811022@hotmail.com>
2020-11-21 10:43:53 +0800

    獨立一個function去做資料庫連線的動作

--- ClassLibrarySQL/Class1.cs
+++ ClassLibrarySQL/Class1.cs
@@ -1,10 +1,25 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace ClassLibrarySQL
 {
     public class Class1
     {
+        // test demo sql connect
+        string constr = @"Data Source=MSI\SQLEXPRESS;Initial Catalog=Northwind;Integrated Security=True";
+        public void executeSql(string sql)
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = constr;
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+            con.Close();
 
+
+
+
+        }
 
 
     }
